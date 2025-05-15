7db3441b0466fc427b4304b965d6eb6518d7cce7
WellNoteOne <165422516+WellNoteOne@users.noreply.github.com>
2025-05-15 12:52:08 +0300

    Adding code for dist

--- index.js
+++ index.js
@@ -96,9 +96,9 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.use(express.static("dist"));
 app.use(unknownEndpoint);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is runninig on port ${PORT}`);
 });
+app.use(express.static("dist"));
